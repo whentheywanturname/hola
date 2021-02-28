4a6404d95ff54fae8cf74199c41babd56d038a2c
whentheywanturname <75698798+whentheywanturname@users.noreply.github.com>
2021-02-27 23:39:13 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canvas;
+var quiz,student, game;
+function setup(){
+  canvas = createCanvas(850,400);
+database = firebase.database();
+
+ 
+}
+
+
+function draw(){
+  background("pink");
+ 
+  
+}
